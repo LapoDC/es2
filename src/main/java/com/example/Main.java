0e60e61b04779e67102ef40aa399228f1e45ec09
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 10:42:16 +0200

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,18 +9,33 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("server avviato");
-        ServerSocket s1 = new ServerSocket(63000);
+        ServerSocket s1 = new ServerSocket(9000);
         Socket s = s1.accept();
         System.out.println("un client si è collegato");
         BufferedReader in= new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String stringaRicevuta=in.readLine();
-        System.out.println("La dtringa ricevuta"+ stringaRicevuta);
+        
+        while (true) 
+        
+        {
+            String stringaRicevuta=in.readLine();
+            System.out.println("La stringa ricevuta "+ stringaRicevuta);
 
-        String stringaMaiuscola=stringaRicevuta.toUpperCase();
-        out.writeBytes(stringaMaiuscola+'\n');
-
+            String stringaMaiuscola=stringaRicevuta.toUpperCase();
+            
+            if(stringaRicevuta.equals("!"))
+            
+            break;
+            
+            out.writeBytes(stringaMaiuscola+'\n');
+            
+            
+            
+        
+        }
+        System.out.println("CONNESSIONE INTERROTTA");
+       
         s.close();
         s1.close();
     }
